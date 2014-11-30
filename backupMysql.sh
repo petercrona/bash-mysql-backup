a9b9c0012d8930998ead36008f91c2be5edcfd2d
Peter Crona <petercrona89@gmail.com>
2014-11-30 17:31:49 +0100

    Added 7z of dates

--- backupMysql.sh
+++ backupMysql.sh
@@ -9,16 +9,17 @@
 USERNAME="root";
 PASSWORD="test";
 BASE_FOLDER="/home/peter/Skrivbord/test";
-EXPECTED_TABLES=161; # Set ridicously high if you don't care. Else check bottom of script.
+EXPECTED_TABLES=160; # Set really low if you don't care. Else check bottom of script.
 EMAIL="petercrona89@gmail.com";
+PASSWORD_7Z="test" # Password to encrypt compressed files with
 
 # Delete files/folders older than a week
 find $BASE_FOLDER/data -mindepth 1 -mtime +7 | xargs rm -Rf
 
 # Create folder for today
 TODAY_FOLDER="$BASE_FOLDER/data/$(date +%Y-%m-%d)";
-if [ -d "$TODAY_FOLDER" ]; then
-    echo "Folder for the current day already existed!" | mail -s "DB Backup service" $EMAIL
+if [ -f "$TODAY_FOLDER.7z" ]; then
+    echo "File for the current day already exist!" | mail -s "DB Backup service" $EMAIL
     exit 1;
 else
     mkdir $TODAY_FOLDER;
@@ -41,8 +42,10 @@ if [[ $EXPECTED_TABLES -gt $NUMBER_OF_TABLES ]]; then
     exit 1;
 fi
 
-
+# Run 7z and remove folders
+7z a -p$PASSWORD_7Z $TODAY_FOLDER.7z $TODAY_FOLDER;
+rm -rf $TODAY_FOLDER;
 
 # ===================================================================================
-# One way to get the number of expected tables is to set it to something really high and then
-# run "find ./data | wc -l" in the terminal. Set the expected to this value.
+# One way to get the number of expected tables is to set it to something really low, comment out the "rm -rf $TODAY_FOLDER"-line
+# and then run "find ./data/<SOME_EXISTING_DATE> | wc -l" in the terminal. Set the expected to this value.
